cfb9c78e95404623e77482098835c217a51eae87
rbsantos-code <richardbriansantos@Richards-MacBook-Pro.local>
2021-06-12 19:38:30 -0700

    testing styles

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      Hello there!
+    <div class="d-flex h-100 text-center text-white bg-dark">
+      <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+      <header class="mb-auto">
+            <div>
+                <h3 class="float-md-start mb-0" id="full-name">Richard Brian Santos</h3>
+                <nav class="nav nav-masthead justify-content-center float-md-end">
+                    <a class="nav-link active" aria-current="page" href="#bio">About Me</a>
+                    <a class="nav-link" href="#projects">Projects</a>
+                    <a class="nav-link" href="https://github.com/rbsantos-code">GitHub</a>
+                    <a class="nav-link" href="#contact">Contact</a>
+                </nav>
+            </div>
+        </header>
+      </div>
     </div>
   );
 }
